ee75fbef5a577aafd7944ba6971421a6b7d88044
reem <144600489+ReemHasanA@users.noreply.github.com>
2025-03-25 06:01:20 +0000

    edit the shipment quantity to be fixed =1

--- app/Listeners/SendOrderToDeliveryService.php
+++ app/Listeners/SendOrderToDeliveryService.php
@@ -80,7 +80,7 @@ class SendOrderToDeliveryService
             'IsReturn'          => false,
             'ShipmentContains'  => $this->formatShipmentContents($order->orderItems),
             'lang'              => 'en', //$preferredLanguage,
-            'ShipmentQuantity'  => $this->getShipmentQuantity($order->orderItems),
+            'ShipmentQuantity'  => 1, //$this->getShipmentQuantity($order->orderItems),
             'IsForeign'         => false,
         ];
 
