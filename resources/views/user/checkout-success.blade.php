f49c8f43d12d82aaa0a237eab8f407261a33bc3e
reem <144600489+ReemHasanA@users.noreply.github.com>
2025-04-06 06:23:19 +0000

    add translate to checkout success

--- resources/views/user/checkout-success.blade.php
+++ resources/views/user/checkout-success.blade.php
@@ -16,9 +16,9 @@
     <div class="text-center">
         <img src="{{ asset('user/img/com-notebook.png') }}" alt="Thank You" class="img-fluid mb-4" style="max-width: 100%; height: auto;">
 
-        <h2 class="text-success">Thank You for Your Order!</h2>
-        <p class="text-muted">You will receive your order within two or three days.</p>
-        <p class="text-secondary">You will receive a confirmation email shortly with the details of your purchase.</p>
+        <h2 class="text-success">{{__('Thank You for Your Order')}}!</h2>
+        <p class="text-muted">{{__('You will receive your order within two or three days')}}.</p>
+        <p class="text-secondary">{{__('You will receive a confirmation email shortly with the details of your purchase')}}.</p>
 
         <div class="mt-4">
             <a href="{{ route('shop.index') }}" class="btn btn-primary">Continue Shopping</a>
