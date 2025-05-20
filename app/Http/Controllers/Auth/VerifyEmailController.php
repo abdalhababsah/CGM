678489f3d7cbbb87ac8272b6dc0e7e4604bbaa2d
ReemHasanA <reem.abdalla98@gmail.com>
2025-05-20 15:57:25 +0300

    fix Route [dashboard] not defined.

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -14,14 +14,16 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
+        $redirectUrl = url('/dashboard?verified=1');
+
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('user.dashboard', absolute: false).'?verified=1');
+            return redirect()->intended($redirectUrl);
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(route('user.dashboard', absolute: false).'?verified=1');
+        return redirect()->intended($redirectUrl);
     }
 }
